188c3d5209144b1bbb8cfa3b406a28cea3671c7f
Daniel Yim <danielyim@gmail.com>
2016-07-24 01:44:19 -0700

    adding test template

--- src/btree.spec.js
+++ src/btree.spec.js
@@ -1,9 +1,43 @@
+'use strict';
+
 import test from 'ava';
+import BTree from './btree';
+import TreeNode from './btree-node';
 
-test('my passing test', t => {
-  t.pass();
+test.before(t => {
+    // this runs before all tests
 });
 
-test('my passing test2', t => {
-  t.pass();
+test.after('cleanup', t => {
+    // this runs after all tests
 });
+
+test.beforeEach(t => {
+    // this runs before each test
+});
+
+test('creates binary tree', t => {
+  var someValue = 'val';
+  var node = new BTree(someValue);
+  if(node.root === someValue) {
+  	t.pass();
+  }
+  else {
+  	t.fail();
+  }
+});
+
+test('creates binary tree node', t => {
+	var someValue = 'val';
+  var node = new TreeNode(someValue);
+  if(node.left !== undefined &&
+  	node.right !== undefined &&
+  	node.value === someValue) {
+  	t.pass();
+  }
+  else {
+  	t.fail();
+  }
+});
+
+test.todo('will think about writing this later');
