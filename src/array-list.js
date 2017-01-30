930f32477671fc159110c76bcf2a68a79d12cbd8
Daniel Yim <danielyim@gmail.com>
2017-01-29 21:49:16 -0800

    array list completed! 100% coverage

--- src/array-list.js
+++ src/array-list.js
@@ -1,5 +1,12 @@
 /**
- * ArrayList
+ * Array List
+ * =============================================================================
+ * An array list dynamically resizes (by doubling) when the number of items
+ * reaches the size of the allocated array
+ *
+ * Since JS arrays don't have a fixed size, we'll try to mimic an ArrayList by
+ * pre-populating it to length n with null and only allowing list operations
+ * where nulls exist.
  */
 export class ArrayList {
   constructor() {
@@ -7,78 +14,109 @@ export class ArrayList {
     this.lastIndex = -1;
   }
 
+  /**
+   * Add a value to the ArrayList.
+   *
+   * It will double the array's working size as soon as it reaches
+   * @param {[type]} value [description]
+   */
   add(value) {
-    // Since JS arrays don't have a fixed size, we'll try to mimic an ArrayList
-    // by pre-populating it to length n with null and only allowing list
-    // operations where nulls exist
-    // if(this.count() === 0) {
-    //   // This is the first element to be added, so fill to n = 2
-    //   this.fillToLength(2);
-    //   this.items[0] = value;
-    //   this.lastIndex = 0;
-    // }
-    // else {
-    //   if(this.count() + 1 > this.items.length) {
-    //     // If we're about to reach the end, double the space and append
-    //     this.fillToLength(Math.max(this.items.length, 1) * 2);
-    //   }
-    // }
-
-    console.log('before:', this.toString());
+    // If adding an item exceeds the length of the array..
     if(this.count() + 1 > this.items.length) {
+      // console.log(`breakpoint reached; about to double. curr ${this.items.length}, target ${Math.max(this.items.length, 1) * 2}`);
       // If we're about to reach the end, double the space and append
       this.fillToLength(Math.max(this.items.length, 1) * 2);
-      console.log(`breakpoint reached; about to double. curr ${this.items.length}, target ${Math.max(this.items.length, 1) * 2}`);
     }
-    this.items[this.lastIndex + 1] = value;
-    this.lastIndex++;
-    // console.log(`lastIndex: ${this.lastIndex}`);
-    console.log('after:', this.toString());
-    console.log('______________________');
+    if(this.items[this.lastIndex + 1] === null) {
+      this.items[this.lastIndex + 1] = value;
+      this.lastIndex++;
+    }
   }
 
   /**
-   * Artifically lengthens the array by filling with nulls
-   * @param  {Number} length Length to fill to
+   * Artifically "lengthens" the array list by filling the workable areas of the
+   * array with nulls
+   * @param  {Number} targetLen Length to fill
    */
-  fillToLength(length) {
-    if(length < this.items.length) {
+  fillToLength(targetLen) {
+    if(targetLen < this.items.length) {
       throw new Error('Target length cannot be less than current length');
     }
-    let currentLength = this.items.length;
-    for(let k = 0; k < length - currentLength; k++) {
+    let currLen = this.items.length;
+    for(let k = 0; k < targetLen - currLen; k++) {
       this.items.push(null);
-      // console.log(`k: ${k}, length: ${length}, this.items.length: ${this.items.length}`);
-      // console.log(this.items);
     }
-    // this.items.fill(null, this.items.length - 1, length - this.items.length);
   }
 
-  remove(value) {
-
+  /**
+   * Artifically "shortens" the array list
+   * @param  {Number} targetLen Length to fill
+   */
+  truncToLength(targetLen) {
+    if(targetLen > this.items.length) {
+      throw new Error('Target length cannot be greater than current length');
+    }
+    if(targetLen <= this.count()) {
+      throw new Error('Target length cannot be less than or equal to the workable area');
+    }
+    let currLen = this.items.length;
+    let value = null;
+    for(let k = 0; k < currLen - targetLen; k++) {
+      value = this.items.pop();
+    }
   }
 
-  removeRange(firstIndex, lastIndex) {
+  /**
+   * Removes and returns the value at the index provided
+   * @param  {Number} index Index
+   * @return {Object}       Item removed at index
+   */
+  removeAt(index) {
+    if(index >= this.count()) {
+      throw new Error('Invalid index');
+    }
 
+    // Remove the element and append a null at the end
+    const value = this.items.splice(index, 1)[0];
+    this.items.push(null);
+    this.lastIndex--;
+
+    // If the workable area is less than half of the total allocated array,
+    // truncate the size
+    if(this.count() < this.items.length / 2) {
+      this.truncToLength(this.items.length / 2);
+    }
+    return value;
   }
 
+  /**
+   * Clears the array list
+   */
   clear() {
     for(let k = 0; k < this.items.length; k++) {
       delete this.items[k];
     }
+    this.lastIndex = -1;
   }
 
+  /**
+   * Returns the count of items in the workable area
+   * @return {Number} Number of items in the workable area
+   */
   count() {
     return this.lastIndex + 1;
   }
 
+  /**
+   * Pretty prints the array list
+   * @param  {String} delimiter Delimiter
+   * @return {String}           String representation of the array list
+   */
   toString(delimiter = ', ') {
     let returnStr = '';
     this.items.forEach(x => {
-      // console.log('processing', x);
-      returnStr += `${returnStr}${delimiter}${x}`;
+      returnStr += `${delimiter}${x}`;
     });
-    // console.log('returning', returnStr);
     return returnStr.substr(2);
   }
 }
--- src/array-list.spec.js
+++ src/array-list.spec.js
@@ -8,49 +8,117 @@ import { ArrayList } from './array-list';
 test('creates empty array list', t => {
   const list = new ArrayList();
   t.true(list.items.length === 0);
-});
-
-test('reports array list count correctly', t => {
-  const list = new ArrayList();
   t.true(list.count() === 0);
 });
-
 
 test('adds an item to the array list', t => {
   const list = new ArrayList();
   list.add('Tina');
-  // list.add('Fred');
-  // list.add('John');
-  // list.add('Bob');
-  // list.add('Fred');
+  list.add('Fred');
+  list.add('John');
+  list.add('Bob');
   // list.add('Sarah');
-  // list.toString();
-  // t.true(list.count === 0);
+  const result = list.toString();
+  t.true(list.count() === 4);
+  t.true(result.includes('Tina'));
+  t.true(result.includes('Fred'));
+  t.true(result.includes('John'));
+  t.true(result.includes('Bob'));
 });
 
 test('fills the array to the appropriate length with nulls', t => {
   const list = new ArrayList();
   list.fillToLength(10);
   t.true(list.items.length === 10);
-  t.true(list.items.filter(x => x !== null).length === 0); // check for nulls
+  // Make sure everything is nulled out
+  t.true(list.items.filter(x => x !== null).length === 0);
   t.true(list.count() === 0); // 0 because we only filled and didn't populate
 });
 
-// test('removes an item from the array list', t => {
-//   const list = new ArrayList();
-//   t.true(list.count() === 0);
-// });
+test('throws an error when an invalid fill length is provided', t => {
+  const list = new ArrayList();
+  list.add('Tina');
+  list.add('Fred');
+  const error = t.throws(() => {
+    list.fillToLength(1);
+  }, Error);
+  t.is(error.message, 'Target length cannot be less than current length');
+});
 
-// test('clears the array list correctly after adding items', t => {
-//   const list = new ArrayList();
-//   list.add('Tina');
-//   list.add('John');
-//   list.add('Bob');
-//   list.add('Fred');
-//   list.add('Sarah');
-//   list.clear();
-//   t.true(list.count() === 0);
-// });
+test('truncates the array to the appropriate length', t => {
+  const list = new ArrayList();
+  list.fillToLength(10);
+  list.truncToLength(5);
+  t.true(list.items.length === 5);
+  // Make sure everything is nulled out
+  t.true(list.items.filter(x => x !== null).length === 0);
+  t.true(list.count() === 0); // 0 because we only filled and didn't populate
+});
+
+test('throws an error when an invalid truncation length is provided', t => {
+  const list = new ArrayList();
+  list.fillToLength(2);
+  const error = t.throws(() => {
+    list.truncToLength(5);
+  }, Error);
+  t.is(error.message, 'Target length cannot be greater than current length');
+});
+
+test('throws an error when attempting to truncate to an invalid size', t => {
+  const list = new ArrayList();
+  list.add('Tina');
+  list.add('Fred');
+  list.add('John');
+  list.add('Bob');
+  const error = t.throws(() => {
+    list.truncToLength(3);
+  }, Error);
+  t.is(error.message, 'Target length cannot be less than or equal to the workable area');
+});
+
+test('clears the array list to an empty state after adding items', t => {
+  const list = new ArrayList();
+  list.add('Tina');
+  list.add('John');
+  list.add('Bob');
+  list.add('Fred');
+  list.add('Sarah');
+  list.clear();
+  t.true(list.count() === 0);
+});
+
+test('removes an item from the array list at specified index and truncates properly', t => {
+  const list = new ArrayList();
+  list.add('Tina');
+  list.add('John');
+  list.add('Bob');
+  list.add('Fred');
+  list.add('Sarah');
+  // console.log(list.items);
+  t.true(list.items.length === 8);
+  t.true(list.removeAt(1) === 'John');
+  t.true(list.removeAt(2) === 'Fred');
+  t.true(list.items.length === 4); // Truncation
+  t.true(list.removeAt(0) === 'Tina');
+});
+
+test('throws an error if an invalid index is provided when calling removeAt', t => {
+  const list = new ArrayList();
+  list.add('Tina');
+  const error = t.throws(() => {
+    // Index that's completely out of bounds of the defined array
+    list.removeAt(3);
+  }, Error);
+
+  list.add('George');
+  const error2 = t.throws(() => {
+    // Index that's out of bounds of the workable area (array size is 4)
+    list.removeAt(2);
+  }, Error);
+  t.is(error.message, 'Invalid index');
+  t.is(error2.message, 'Invalid index');
+});
+
 
 // test('returns the correct string representation of an array list with and without a custom delimiter', t => {
 //   const list = new ArrayList();
