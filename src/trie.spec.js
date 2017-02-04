9fd2d2ab474ff5dd6945ad636e67be4629cdd2a2
Daniel Yim <danielyim@gmail.com>
2017-02-04 01:01:29 -0800

    more skeletons for tries

--- src/trie.spec.js
+++ src/trie.spec.js
@@ -5,4 +5,29 @@ import Trie from './trie';
 // test.after('cleanup', t => { ... });
 // test.beforeEach(t => { ... });
 
-test.todo('should test tries');
+test('should initialize trie', t => {
+  const tr = new Trie();
+  t.true(true);
+});
+
+test('should add to a trie', t => {
+  const tr = new Trie();
+  const value = 'test';
+  tr.add(value);
+  t.true(true);
+});
+
+test('should search from trie', t => {
+  const tr = new Trie();
+  const value = 'test';
+  const result = tr.search(value);
+  t.true(true);
+});
+
+test('should remove a value from trie', t => {
+  const tr = new Trie();
+  tr.remove('test');
+  t.true(true);
+});
+
+test.todo('should test trie properties');
